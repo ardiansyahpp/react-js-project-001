68ab81460143187d5c50949ece53fc1ef9639c0c
ardiansyahpp <ardiansyahputrap@gmail.com>
2021-01-15 03:31:57 +0700

    fixing usestate issue within cards page

--- src/Pages/Cards.jsx
+++ src/Pages/Cards.jsx
@@ -1,19 +1,16 @@
-import { useState } from "react";
+import Cardspage from "../components/CardsPages/Cardspages"
 
 const Cards = () => {
-
-    const [change, setChange] = useState(false)
-
-    const handleClick = () => {
-        setChange(!change)
-    }
-
     return (
-        <div className="cards-container">
-            <div className={change ? "cards-content" : 'cards-content-blue'} onClick={handleClick} ></div>
-            <div className={change ? "cards-content" : 'cards-content-blue'}></div>
-            <div className="cards-content"></div>
-            <div className="cards-content"></div>
+        <div className="cards__page">
+            <Cardspage name={"Person 1"}
+                sourceimage={'https://img4.wikia.nocookie.net/__cb20120815160236/simpsons/images/0/01/200px-Langdon_Alger.png'} />
+            <Cardspage name={"Person 2"}
+                sourceimage={'https://img1.wikia.nocookie.net/__cb20110929163657/simpsons/images/d/db/200px-Chuck_Muntz.png'} />
+            <Cardspage name={"Person 3"}
+                sourceimage={'https://img4.wikia.nocookie.net/__cb20120815160236/simpsons/images/0/01/200px-Langdon_Alger.png'} />
+            <Cardspage name={"Person 4"}
+                sourceimage={'https://img1.wikia.nocookie.net/__cb20110929163657/simpsons/images/d/db/200px-Chuck_Muntz.png'} />
         </div>
     );
 }
